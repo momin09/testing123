db692b723dbf76d8e07fef0cbe5433dec74c321a
momin09 <khm0909z@gmail.com>
2021-11-03 15:27:40 +0900

    Modify: main.go 'why...'

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("plz..."))
+		w.Write([]byte("why..."))
 	})
 
 	http.ListenAndServe(":8080", nil)
