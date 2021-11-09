6d25e454aabe2ed41ca5a00bd983c0171e3dea49
momin09 <khm0909z@gmail.com>
2021-11-09 14:41:27 +0900

    Modify: add uri

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import "net/http"
 
 func main() {
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello? Strange?"))
+		w.Write([]byte("Hello?"))
+	})
+
+	http.HandleFunc("/world", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Is that u?"))
 	})
 
 	http.ListenAndServe(":8080", nil)
